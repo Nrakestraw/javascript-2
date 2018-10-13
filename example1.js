600cbcfa46a46a74424447f4b5b90ecdeb40507f
Nate Rakestraw <nathan@rakestraw.org>
2018-10-13 10:40:20 -0500

    for loops, while loops, and if statements.

---
+++ example1.js
@@ -0,0 +1,25 @@
+// var number= 99;
+// var message= "bottles of beer on the wall"
+// var message2= "bottles of beer"
+// var message3= "take one down pass it around"
+
+// while(number >=0){
+//     console.log(number + " " + message + " " + number + " " + message2 + " " + message3 + " " + number + " " + message);
+//     number--;
+// }
+
+for( var num=99; num >0 ; num--){
+
+  console.log(num + " Bottles of beer on the wall");
+  console.log(num + " Bottles of beer");
+  console.log(" take one down, pass it around");
+  console.log(num + " bottles of beer on the wall");
+  if ((num - 1) != 0){
+  console.log((num -1) + " bottles of beer on the wall)")}
+
+else {
+    console.log("No more bottles of beer on the wall");
+}
+console.log("");
+
+}
