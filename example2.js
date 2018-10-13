a77bbe7a5bea2cddc0f43994ff4fc345ffc52c6e
Nate Rakestraw <nathan@rakestraw.org>
2018-10-13 11:33:44 -0500

    truthyness and function examples

---
+++ example2.js
@@ -0,0 +1,27 @@
+
+function makeMessage(test){
+    if(test){
+
+    console.log("I called a function");}
+    else {
+
+        console.log("I called a function differently")
+    }
+
+}
+
+makeMessage(true);
+makeMessage();
+makeMessage();
+makeMessage();
+makeMessage();
+makeMessage();
+
+
+
+// for(var num1 = 1; num1 < 9; num1++){
+//     message= ""
+//     for(var num2=1; num2 <= num1; num2++){
+//     message = message + num2 + " ";}
+//    console.log(message);
+// }
