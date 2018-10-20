c31269fc71b989cd092ab985ed7787bfecbc38cf
Nate Rakestraw <nathan@rakestraw.org>
2018-10-20 11:53:50 -0500

    Week-3

--- example2.js
+++ example2.js
@@ -1,27 +1,56 @@
+// var table= [
+//     ["3", "4", "2", "9"],
+//     ["nate", "terry", "bob", "chris"],
+//     ["hello", "bye", "seeya", "later"],
+//     ["java", "script", "fun", "whoa"],
+// ]
+// for(i= 0; i< table.length; i++){
+//     for(j= 0; j< table[i].length; j++){
+//         console.log(table[i][j]);
+//     }
+// }
 
-function makeMessage(test){
-    if(test){
+// var person1= {
+//     firstname: "Nate",
+//     lastname: "Rakestraw",
+//     age: 30
+// }
 
-    console.log("I called a function");}
-    else {
+// var person2= {
+//     firstname: "Terry",
+//     lastname: "Brown",
+//     age: 30
+// }
 
-        console.log("I called a function differently")
+// var person3= {
+//     firstname: "Sarah",
+//     lastname: "Natalie",
+//     age: 30
+// }
+
+// var people= [ person1, person2, person3]
+
+// console.log(people)
+
+var maths = {
+    add: function(x,y){
+        var sum = x + y;
+        return sum
+    },
+    subtract: function(x,y){
+        var sub = x - y;
+        return sub
+    },
+    divide: function(x,y){
+        var div= x / y;
+        return div
+
+
+    },
+    multiply: function(x,y){
+        var mult = x * y;
+        return mult
     }
-
 }
 
-makeMessage(true);
-makeMessage();
-makeMessage();
-makeMessage();
-makeMessage();
-makeMessage();
-
-
-
-// for(var num1 = 1; num1 < 9; num1++){
-//     message= ""
-//     for(var num2=1; num2 <= num1; num2++){
-//     message = message + num2 + " ";}
-//    console.log(message);
-// }
+console.log(maths.add(3,4))
